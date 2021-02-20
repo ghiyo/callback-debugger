6804807223ae9f9cba49875872fbb82bacf29815
Malush <o.ghiyasi@gmail.com>
2021-02-20 18:19:34 -0500

    formatting

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/callback', (req, res) => {
-  console.log(req.body);
-  console.log(req.query);
-  console.log(req.params);
+  console.log(`Body:\n-----\n${JSON.stringify(req.body, null, 2)}\n`);
+  console.log(`Query:\n-----\n${JSON.stringify(req.query, null, 2)}\n`);
+  console.log(`Params:\n-----\n${JSON.stringify(req.params, null, 2)}`);
+  console.log('-----------------------------------------------------------');
   res.sendStatus(200);
 })
 
